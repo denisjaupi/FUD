30dfaf124d691ac8f18e457c8f46324ec0a8403b
dario <dario.mori@stud.unifi.it>
2024-05-10 16:35:57 +0200

    Connection database

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import Model.Database.Db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Create a new Home window
         new View.Home();
+        ResultSet rs = Db.result("Select * from exercise");
+        rs.absolute(1);
+        System.out.println(rs.getString("name"));
     }
 }
